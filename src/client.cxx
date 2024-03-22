a947660c0193efe04633dc4352118533526811a4
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2024-03-22 12:07:36 -0700

    Switch vcpkg for git modules

--- src/client.cxx
+++ src/client.cxx
@@ -1,34 +1,30 @@
-#include <string>
 #include <iostream>
+#include <string>
 
+#include <fmt/core.h>
 #include <zmq.hpp>
 
-int main()
-{
-    // initialize the zmq context with a single IO thread
-    zmq::context_t context{1};
+int main() {
+  // initialize the zmq context with a single IO thread
+  zmq::context_t context{1};
 
-    // construct a REQ (request) socket and connect to interface
-    zmq::socket_t socket{context, zmq::socket_type::req};
-    socket.connect("tcp://localhost:5555");
+  // construct a REQ (request) socket and connect to interface
+  zmq::socket_t socket{context, zmq::socket_type::req};
+  socket.connect("tcp://localhost:5555");
 
-    // set up some static data to send
-    const std::string data{"Hello"};
+  for (auto request_num = 0; request_num < 1000000; ++request_num) {
+    double *ptr = (double *)malloc(sizeof(double));
+    *ptr = (double)request_num;
+    zmq::mutable_buffer msg_data = zmq::buffer(ptr, sizeof(double));
 
-    for (auto request_num = 0; request_num < 10; ++request_num)
-    {
-        // send the request message
-        std::cout << "Sending Hello " << request_num << "..." << std::endl;
-        socket.send(zmq::buffer(data), zmq::send_flags::none);
-
-        // wait for reply from server
-        zmq::message_t reply{};
-        auto out = socket.recv(reply, zmq::recv_flags::none);
-
-        std::cout << "Received " << reply.to_string();
-        std::cout << " (" << request_num << ")";
-        std::cout << std::endl;
+    socket.send(msg_data, zmq::send_flags::none);
+    zmq::message_t reply{};
+    auto out = socket.recv(reply, zmq::recv_flags::none);
+    auto data = *(double *)(reply.data());
+    if (request_num % 10000 == 0) {
+      fmt::print("data = {}\n", data);
     }
+  }
 
-    return 0;
+  return 0;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -7,66 +7,55 @@
 #include "mex.hpp"
 #include "mexAdapter.hpp"
 
+#include <fmt/core.h>
 #include <string>
 #include <zmq.hpp>
-#include <fmt/core.h>
 
 using namespace matlab::data;
 using matlab::mex::ArgumentList;
 
-class MexFunction : public matlab::mex::Function
-{
-public:
-    void operator()(ArgumentList outputs, ArgumentList inputs)
-    {
-        checkArguments(outputs, inputs);
-        TypedArray<double> doubleArray = std::move(inputs[0]);
+class MexFunction : public matlab::mex::Function {
+ public:
+  void operator()(ArgumentList outputs, ArgumentList inputs) {
+    checkArguments(outputs, inputs);
+    TypedArray<double> doubleArray = std::move(inputs[0]);
 
-        // initialize the zmq context with a single IO thread
-        zmq::context_t context{1};
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
 
-        // construct a REQ (request) socket and connect to interface
-        zmq::socket_t socket{context, zmq::socket_type::req};
-        socket.connect("tcp://localhost:5555");
+    // construct a REQ (request) socket and connect to interface
+    zmq::socket_t socket{context, zmq::socket_type::req};
+    socket.connect("tcp://localhost:5555");
 
-        for (auto &elem : doubleArray)
-        {
-            // std::cout << "Loop " << elem << "\n";
-            void *ptr = malloc(sizeof(double));
-            ptr = &elem;
-            zmq::mutable_buffer msg_data = zmq::buffer(ptr, sizeof(double));
+    for (auto &elem : doubleArray) {
+      // std::cout << "Loop " << elem << "\n";
+      void *ptr = malloc(sizeof(double));
+      ptr = &elem;
+      zmq::mutable_buffer msg_data = zmq::buffer(ptr, sizeof(double));
 
-            socket.send(msg_data, zmq::send_flags::none);
-            zmq::message_t reply{};
-            auto out = socket.recv(reply, zmq::recv_flags::none);
-            elem = *(double *)(reply.data());
-            // std::cout << "elem " << elem << "\n";
-        }
-        outputs[0] = doubleArray;
+      socket.send(msg_data, zmq::send_flags::none);
+      zmq::message_t reply{};
+      auto out = socket.recv(reply, zmq::recv_flags::none);
+      elem = *(double *)(reply.data());
+      // std::cout << "elem " << elem << "\n";
     }
+    outputs[0] = doubleArray;
+  }
 
-    void checkArguments(ArgumentList outputs, ArgumentList inputs)
-    {
-        // Get pointer to engine
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+  void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+    // Get pointer to engine
+    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
 
-        // Get array factory
-        ArrayFactory factory;
+    // Get array factory
+    ArrayFactory factory;
 
-        // Check array argument: Second input must be double array
-        if (inputs[0].getType() != ArrayType::DOUBLE ||
-            inputs[0].getType() == ArrayType::COMPLEX_DOUBLE)
-        {
-            matlabPtr->feval(u"error",
-                             0,
-                             std::vector<Array>({factory.createScalar("Input must be double array")}));
-        }
-        // Check number of outputs
-        if (outputs.size() > 1)
-        {
-            matlabPtr->feval(u"error",
-                             0,
-                             std::vector<Array>({factory.createScalar("Only one output is returned")}));
-        }
+    // Check array argument: Second input must be double array
+    if (inputs[0].getType() != ArrayType::DOUBLE || inputs[0].getType() == ArrayType::COMPLEX_DOUBLE) {
+      matlabPtr->feval(u"error", 0, std::vector<Array>({factory.createScalar("Input must be double array")}));
     }
+    // Check number of outputs
+    if (outputs.size() > 1) {
+      matlabPtr->feval(u"error", 0, std::vector<Array>({factory.createScalar("Only one output is returned")}));
+    }
+  }
 };
